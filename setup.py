9dfe6e9fdb21d67698a511bb6785bfb28dcf1de1
MaksHess <max.hess@imls.uzh.ch>
2023-10-27 12:40:18 +0200

    fix readme.rst

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 
 from setuptools import find_packages, setup
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
+# with open("HISTORY.rst") as history_file:
+#     history = history_file.read()
 
 requirements = [
     "h5py",
@@ -53,7 +53,7 @@ setup(
     # },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="tracking",
     name="tracking",
